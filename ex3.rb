36f5d7a84780a80e9feadf2fd1a3c49a87c07387
MelipeFoon <felipe.t.moon@gmail.com>
2024-09-20 17:41:08 -0300

    exercise 3

---
+++ ex3.rb
@@ -0,0 +1,33 @@
+# print the string written
+puts "I will now count my chickes:"
+
+# print the frases with string interpolation, calculates the expression inside de curly brackets.
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# print the string written
+puts "Now I will count the eggs:"
+
+# calculate and print the result of the expression
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+
+# print the string written
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# print a boolean value, checks if the expressions in the left is less than the expression in the right
+puts 3 + 2 < 5 - 7
+
+# print the frases with string interpolation, calculates the expression inside de curly brackets.
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7 #{5 - 7}"
+
+# print the string written
+puts "Oh, that's why it's false."
+
+# print the string written
+puts "How about some more."
+
+# print the frases with string interpolation, calculates the expression inside de curly brackets.
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"

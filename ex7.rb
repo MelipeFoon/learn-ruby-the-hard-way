8257483a1839692154e16597ae7291400ba575c5
MelipeFoon <felipe.t.moon@gmail.com>
2024-09-21 19:11:32 -0300

    exercise 7

---
+++ ex7.rb
@@ -0,0 +1,26 @@
+# print the string written
+puts "Marry had a little lamb." 
+# print the frase with string interpolation.
+puts "It's fleece was white as #{'snow'}."
+# print the string written
+puts "And everywhere that Marry went."
+# print the string written, but the "* 10" makes it repeat 10 times
+puts "." * 10
+
+# assign a string to a variable.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print vs puts
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12

0497a97197f7a232ca83ee6a132ea3f247ef456e
MelipeFoon <felipe.t.moon@gmail.com>
2024-09-21 20:11:52 -0300

    exercise 11

---
+++ ex11.rb
@@ -0,0 +1,12 @@
+print "How old are you? "
+age = gets.chomp # "gets.chomp" makes it possible to receive some user input into the code.
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weight? "
+weight = gets.chomp
+
+puts "So you are #{age} old, #{height} tall and #{weight} heavy."
+
+print "Are you a male or female? "
+gender = gets.chomp
+puts "So you are a #{gender}."
